99260c75ebd5251592491b339512416ce5bfe7ea
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2022-11-30 17:50:34 +0100

    Module-1: Introduction to Spring Boot

---
+++ src/main/java/com/programming/techie/springtutorialblog/SpringTutorialBlogApplication.java
@@ -0,0 +1,13 @@
+package com.programming.techie.springtutorialblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programming/techie/springtutorialblog/SpringTutorialBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.techie.springtutorialblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
